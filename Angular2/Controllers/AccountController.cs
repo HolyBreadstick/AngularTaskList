b64b6922eb765ee14d5e27ffc3b0d18aedd1dc11
Bailey Miller <bjm19@pct.edu>
2017-04-18 13:37:43 -0400

    Working on creating the structure

---
+++ Angular2/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public Object Login() {
+            return "";
+        }
+
+        public Object Register() {
+            return "";
+        }
+    }
+
+    public class LoginResponse : RootResponse {
+        
+        public String Token { get; set; }
+
+        public LoginResponse() : base() {
+            
+        }
+
+        public LoginResponse(String des) : base(des)
+        {
+
+        }
+    }
+
+    public class RootResponse {
+
+        private Stopwatch timer = new Stopwatch();
+
+        public RootResponse() {
+            timer.Stop();
+        }
+
+        public RootResponse(String des) {
+            timer.Start();
+            Description = des;
+        }
+
+        private TimeSpan CalculateExecutionTimer() {
+            timer.Stop();
+            return timer.Elapsed;
+        }
+
+        public List<Error> Errors { get; set; } = new List<Error>();
+        public TimeSpan ExecutionTime { get {
+                return CalculateExecutionTimer();
+            } set {
+                this.ExecutionTime = value;
+            } }
+        public String Description { get; set; } = "Root response object";
+
+        public DateTime TimeRequested { get; set; } = DateTime.UtcNow;
+
+        public void AddError(String m, int level = 1) {
+            Errors.Add(new Error()
+            {
+                Message = m,
+                ErrorLevel = level
+            });
+        }
+        
+    }
+    public class Error {
+        public String Message { get; set; } = "No message available";
+        [Range(1, 10)]
+        public int ErrorLevel { get; set; } = 1;
+    }
+}
